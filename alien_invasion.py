fc091d3dd6bda98368a0340a255f9923820ce885
dshebeko20 <dshebeko20@gmail.com>
2025-09-03 22:55:07 +0400

    Screen color

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,8 @@ class AlienIvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Запускает осовной цикл игры"""
         while True:
@@ -21,6 +23,9 @@ class AlienIvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # При каждом проходе цикла прерисовывается экран.
+            self.screen.fill(self.bg_color)
+
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
             self.clock.tick(60)
