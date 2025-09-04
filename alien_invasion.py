b802e013c24da495739f9c388a5a131090f483d2
dshebeko20 <dshebeko20@gmail.com>
2025-09-04 16:47:48 +0400

    Class Ship.

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,12 +13,9 @@ class AlienIvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запускает осовной цикл игры"""
