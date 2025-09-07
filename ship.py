8f63cc40778069b9de3718042087ebc3c28d3beb
dshebeko20 <dshebeko20@gmail.com>
2025-09-07 16:30:10 +0400

    The speed of the ship

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         """Инициализирует корабль и задаёт его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загржает изображение корабля и получает прямоугольник.
@@ -14,16 +15,23 @@ class Ship:
         # Каждый новый корабль появляеся у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Флаг преремещения: начинаем с неподвиного корабля.
+        # Сохранение вещественной координаты центра корбля.
+        self.x = float(self.rect.x)
+
+        # Флаги преремещения: начинаем с неподвиного корабля.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """обновляет позицию корабля с учетом флага."""
+        """обновляет позицию корабля с учетом флагов."""
+        # Обновляет фтрибут x, не rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
