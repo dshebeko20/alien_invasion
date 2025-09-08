66119d71edd4a910c622f4e4ea270185376be83b
dshebeko20 <dshebeko20@gmail.com>
2025-09-08 11:39:48 +0400

    Bullet settings.

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # Настройка корабля
         self.ship_speed = 1.5
+        
+        # Папраметры снаряда
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_heigh = 15
+        self.bullet_color = (60, 60, 60)
