6a472f4a56a8f8d319278114a8e7ee3cc562547c
dshebeko20 <dshebeko20@gmail.com>
2025-09-09 14:50:55 +0400

    Processing of shots

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 class AlienIvasion:
     """Класс для управлением ресурсами и поведением игры."""
 
@@ -20,12 +20,14 @@ class AlienIvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запускает осовной цикл игры"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             
             self.clock.tick(60)
@@ -48,7 +50,15 @@ class AlienIvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
-       
+        elif event.key == pygame.K_SPACE:
+            self.fire_bullet()
+
+    def fire_bullet(self):
+        # Создаёт новый снард и добавляет его в группу bullets.
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+    
+    
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш."""       
         if event.key == pygame.K_RIGHT:
@@ -60,6 +70,8 @@ class AlienIvasion:
     def _update_screen(self):
             """Обновляет изображения на экране и отображает новый экран."""
             self.screen.fill(self.settings.bg_color)
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             self.ship.blitme()
 
             pygame.display.flip()
--- settings.py
+++ settings.py
@@ -14,5 +14,5 @@ class Settings:
         # Папраметры снаряда
         self.bullet_speed = 2.0
         self.bullet_width = 3
-        self.bullet_heigh = 15
+        self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
