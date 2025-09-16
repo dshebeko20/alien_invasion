00567b4a7df65ebc6121dd8b405e5682ff77ba6b
dshebeko20 <dshebeko20@gmail.com>
2025-09-16 14:21:35 +0400

    Aliens fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,9 +72,18 @@ class AlienIvasion:
     
     def _create_fleet(self):
         """Создаёт флот пришеьцев."""
-        # Создание пришельца.
+        # Создание пришельца и вычисление количества пришелцев в ряду.
+        # Интервал между соседними пришелььцам равен ширене пришельца.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
     
     def _update_bullets(self):
         """ Обновляет позиции снарядов и уничтожает старые снаряды."""
