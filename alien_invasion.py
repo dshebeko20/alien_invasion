f2edea744b5e8d85a80cbd95898dc55ae22d6206
dshebeko20 <dshebeko20@gmail.com>
2025-09-18 13:00:28 +0400

    Adding rows.

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,21 +72,28 @@ class AlienIvasion:
     
     def _create_fleet(self):
         """Создаёт флот пришеьцев."""
-        # Создание пришельца и вычисление количества пришелцев в ряду.
-        # Интервал между соседними пришелььцам равен ширене пришельца.
+        # Создание пришельца и добавление других, пока остаётся место.
+        # Рассстояние между пришельцами составляет одну ширину 
+        # и одну выстоу пришельца.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            # Конец ряда: сбрасываем значение x  и инкрементируем значение y.
+            current_x = alien_width
+            current_y += 2 * alien_height
            
-    def _create_alien(self, x_position):
+    def _create_alien(self, x_position, y_position):
         """Создаёт пришельца и размещает его в ряду."""
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
         
     def _update_bullets(self):
