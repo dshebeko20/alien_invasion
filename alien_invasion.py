e49cd7dc15f08289d35ba35ef458ba98c78fdedc
dshebeko20 <dshebeko20@gmail.com>
2025-09-29 19:04:02 +0400

    Game over and executble parts.

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,14 +31,18 @@ class AlienIvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+        
+        # Игра "Инопланетное вторжение" запускается в активном состоянии.
+        self.game_active = True
 
     def run_game(self):
         """Запускает осовной цикл игры"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -114,19 +118,22 @@ class AlienIvasion:
 
     def _ship_hit(self):
         """Обрабатыват столкновение корабля с пришельцем."""
-        # Уменьшение ships_left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Уменьшение ships_left.
+            self.stats.ships_left -= 1
 
-        # Очистка групп aliens и bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Очистка групп aliens и bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Создание нового флота и размещение корабля в центре.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Создание нового флота и размещение корабля в центре.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Пауза
-        sleep(0.5)
+            # Пауза
+            sleep(0.5)
+        else:
+            self.game_active = False
 
     def check_aliens_bottom(self):
         """Проверяет, добралиась ли пришельцы до нижнего кра экрана."""
