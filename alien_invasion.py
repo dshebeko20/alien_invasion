5914132224fbb93a7f6067410e27ec1f89408fa5
dshebeko20 <dshebeko20@gmail.com>
2025-10-10 21:42:30 +0400

    The sound of a gunshot

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,6 +19,8 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создаёт игровые ресурсы"""
         pygame.init()
+        pygame.mixer.init()
+        
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
@@ -26,6 +28,8 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
+        self.mixer = pygame.mixer.music.load('sound/SHOOT011.mp3')
 
         # Создание экземпляров для хранения статистики и панели результатов.
         self.stats = GameStats(self)
@@ -120,6 +124,7 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+            pygame.mixer.music.play()
 
     def _update_bullets(self):
         """ Обновляет позиции снарядов и уничтожает старые снаряды."""
